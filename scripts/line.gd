93e1c8b3009824b366a69d43922f1b6647ab798f
evankelch <evan.c.kelch@gmail.com>
2025-03-01 14:58:10 -0500

    upd to subv 4 bc line2d undo was broken in 3, drawing line on bezier path, fixed physics props

--- scripts/line.gd
+++ scripts/line.gd
@@ -1,13 +1,16 @@
 extends StaticBody2D
 
-@onready var line_2d: Line2D = $Line2D
+@onready var path_2d: Path2D = $Path2D
+@onready var visLine: Line2D = $Line2D
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	for i in line_2d.points.size() - 1:
+	var points = path_2d.curve.get_baked_points()
+	visLine.points = points
+	for i in points.size() - 1:
 		var segment = SegmentShape2D.new()
-		segment.a = line_2d.points[i]
-		segment.b = line_2d.points[i + 1]
+		segment.a = points[i]
+		segment.b = points[i + 1]
 		var collider = CollisionShape2D.new()
 		collider.shape = segment
 		self.add_child(collider)
