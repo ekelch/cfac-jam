563048a4c158d5678994f04d102ee1527926842c
evankelch <evan.c.kelch@gmail.com>
2025-03-01 15:19:09 -0500

    for each on bezier drawing lines, background tiling

--- scripts/line.gd
+++ scripts/line.gd
@@ -1,19 +1,22 @@
 extends StaticBody2D
 
-@onready var path_2d: Path2D = $Path2D
-@onready var visLine: Line2D = $Line2D
-
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	var points = path_2d.curve.get_baked_points()
-	visLine.points = points
-	for i in points.size() - 1:
-		var segment = SegmentShape2D.new()
-		segment.a = points[i]
-		segment.b = points[i + 1]
-		var collider = CollisionShape2D.new()
-		collider.shape = segment
-		self.add_child(collider)
+	var lines = get_children()
+	for line in lines:
+		var points = line.curve.get_baked_points()
+		var visibleLine = Line2D.new();
+		visibleLine.points = points
+		visibleLine.width = 1.0
+		visibleLine.antialiased = true
+		self.add_child(visibleLine)
+		for i in points.size() - 1:
+			var segment = SegmentShape2D.new()
+			segment.a = points[i]
+			segment.b = points[i + 1]
+			var collider = CollisionShape2D.new()
+			collider.shape = segment
+			self.add_child(collider)
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
