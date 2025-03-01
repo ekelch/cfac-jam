65cf762aeb59b684153f2861eff99b9be57da836
evankelch <evan.c.kelch@gmail.com>
2025-03-01 11:47:57 -0500

    first impl dyn colliders

--- scripts/line.gd
+++ scripts/line.gd
@@ -1,9 +1,16 @@
 extends StaticBody2D
 
+@onready var line_2d: Line2D = $Line2D
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	print(position)
+	for i in line_2d.points.size() - 1:
+		var segment = SegmentShape2D.new()
+		segment.a = line_2d.points[i]
+		segment.b = line_2d.points[i + 1]
+		var collider = CollisionShape2D.new()
+		collider.shape = segment
+		self.add_child(collider)
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
